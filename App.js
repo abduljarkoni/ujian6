4cc66e43d86bb9dfbb8faec70ee63359bad8334f
abdul jarkoni <abduljarkoni.aj@gmail.com>
2021-02-22 07:23:03 +0700

    ujian ke 6

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Form from './src/components/Form';
+import Home from './src/components/Home';
+import FormUpdate from './src/components/FormUpdate';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import Detail from './src/components/Detail';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Users" component={Home} />
+        <Stack.Screen name="Form" component={Form} />
+        <Stack.Screen name="Update" component={FormUpdate} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
